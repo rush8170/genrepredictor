dd94ae5c23a74279b595f590a52fe510b17e7b53
rushil <shahrushil@yahoo.com>
2019-05-07 00:56:08 +0530

    initial commit

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pickle
 import re
 import nltk
-#nltk.download('stopwords')
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 corpus = []
